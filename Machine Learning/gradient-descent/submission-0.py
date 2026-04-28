288279aa02735e2ddc50c6e4a5b547f96a931a52
Prathvi G Shetty <168317575+BlunderingProgrammmer@users.noreply.github.com>
2026-04-28 14:08:13 +0530

    Remove Legacy Machine Learning/ folder

--- Machine Learning/gradient-descent/submission-0.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        minimizer = init
-        for _ in range(iterations):
-            derivative = 2 * minimizer #error was using const value of init as instead of updating each step
-            minimizer = minimizer - learning_rate * derivative
-        return round(minimizer,5)
