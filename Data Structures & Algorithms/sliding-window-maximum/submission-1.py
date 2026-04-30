06c0b136fd56fe28339ea4b9d813e566699a4fc8
Prathvi G Shetty <168317575+BlunderingProgrammmer@users.noreply.github.com>
2026-04-30 11:38:57 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()
+        l = r = 0
+        while r<len(nums):
+            #store index in not value
+            #while q not empty and top of q smaller than new r introduced
+            #since its a monotonic q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+            # bounds check when window moves that its not still holding invalid value
+            if l > q[0]: #max value always at the 0 position since monotonic que
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l+=1
+            r+=1
+        return output
